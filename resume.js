975edf5f964c72e8481e9d799eb3072d879f537a
KEERTHANADURAISAM <115733037+KEERTHANADURAISAM@users.noreply.github.com>
2022-10-20 20:56:40 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,72 @@
+const personalinfo=
+ 
+[
+    {
+        "name"          :   "D.Keerthana",
+        "Father's name" :   "K.Duraisamy",
+        "Date of Birth" :   "05/11/1999",
+        "Age"	        :	"23",
+        "Gender"	    : 	"Female",
+        "Religion"	    :	"Hindu",
+        "Nationality"	:	"Indian",
+        "Marital status":	"Unmarried",
+        "Languages"	    :	"Tamil, English"
+       
+       
+    },
+]
+const qualification=
+[
+    {
+   "Qualification"   :"BCA-2019",
+   "SOFT SKILLS"     :"C,C++,VB.Net",
+   "TECHNICAL SKILLS":"Tally"
+},
+]
+
+const project=
+[{
+   "PROJECT"  :"Student Internal Mark Assessment System",
+	"Language":" VB.NET",
+	"Database":"-SQL"
+
+},
+]
+const per=[
+    {
+    
+"1":"Positive thinking",
+"2":"Hard Working",
+"3":"Self-Motivation"
+
+}
+]
+const address=[{
+    
+"D/O":" K.Duraisamy",
+"11/254":"Kombaipatti",
+"Kanakkanpati":" (PO)",
+"Palani":"Tk", 					
+"Dindigul":"Dt",
+"pincode":"624213"
+}]
+const contact=[{
+    "Email Id": "tarakeerthu@gmail.com",
+    "ph no":"9788325216"
+}]
+
+
+console.log("personal info:");
+console.log(personalinfo);
+console.log("qualification:");
+console.log(qualification)
+console.log("project:");
+console.log(project);
+console.log("PERSONAL GROWTH:")
+console.log(per);
+console.log("Address:");
+console.log(address);
+console.log("Contact Details:");
+console.log(contact);
+
+
